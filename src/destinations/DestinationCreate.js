baddde431d34e6edf737c2981561b133980fe007
johnulbright <john.ulbright@gmail.com>
2021-02-18 21:59:55 -0500

    destinations getting closer

--- src/destinations/DestinationCreate.js
+++ src/destinations/DestinationCreate.js
@@ -1,18 +1,80 @@
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
 
 const DestinationCreate = (props) => {
-  console.log("pops from create", props.possibleDestinations);
-  //useEffect(() => {}, [props.possibleDestinations]);
+  const [possibleDestinations,setPossibleDestinations]=useState({});
+  console.log("props from create", props);
+  
+  const manageDestinations=()=>{
+    setPossibleDestinations([]);
+    const limit = 7;
+    console.log("trip",props.trip)
+    fetch(
+        `https://api.opentripmap.com/0.1/en/places/radius?radius=100000&lon=${props.trip.lon}&lat=${props.trip.lat}&apikey=${props.api_key}`
+      )
+        .then((res) => res.json())
+        .then((json) => {
+          let filtered = json.features;
+          let haveWikidata = [];
+          for (let i = 0; i < filtered.length; i++) {
+            if (filtered[i].properties.wikidata) {
+              haveWikidata.push(filtered[i]);
+            }
+          }
+          return haveWikidata;
+        })
+        .then((usable) => {
+          let count=0;
+          console.log("limit",limit)
+          //for(let i=0;i<usable.length&&newArray.length<limit;i++){
+          for (let i = 0; i < usable.length&&i<limit ; i++) {
+          // if(count==limit) {break;}
+            fetch(
+              `https://api.opentripmap.com/0.1/en/places/xid/${usable[i].properties.xid}?apikey=${props.api_key}`
+            )
+              .then((res) => res.json())
+              .then((json) => {
+                let img = "";
+                let text = "";
+                if (json.preview) {
+                    if(json.preview.source) {
+                      img = json.preview.source;
+                    }
+                }
+                if (json.wikipedia_extracts) {
+                    if(json.wikipedia_extracts.text){
+                      text = json.wikipedia_extracts.text;
+                    }
+                }
+                console.log("test",img==""||text=="")
+                console.log("count",count);
+                if(img==""||text==""){} else{ 
+                    count++;
+                setPossibleDestinations([{ "name": json.name, "image": img, "descr": text,"wikidata":json.wikidata }].concat(possibleDestinations))
+        
+              };
+          }
+          
+        )
+    }
+  }
+        )
+}
+
+  useEffect(() => {manageDestinations()}, [props.trip]);
+
+  const displayer=(item)=>{
+    return(<div key={item.wikidata}>
+          <h1>{item.name}</h1>
+          <img src={item.image}/>
+          <p>{item.descr}</p>
+
+        </div>)
+  }
 
   return (
-    <div>
-      test1
-      {props.possibleDestinations?.map((entry) => {
-        <div>
-          huh?
-          <h1>{entry.name}</h1>
-        </div>
-      })}
+    <div key={props.trip.id}>
+      This is where you'll pick some destinations.
+      {possibleDestinations.places?.map((entry) => displayer(entry))}
     </div>
   );
 };
---
+++ src/trips/TripIndex.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+import TripCreate from "../trips/TripCreate";
+import TripTable from "../trips/TripTable";
+import TripEdit from "../trips/TripEdit";
+
+
+const TripIndex = (props) => {
+    const [updateActive, setUpdateActive] = useState(false);
+    const [tripToUpdate, setTripToUpdate] = useState({});
+    const [trips, setTrips] = useState([]);
+    const editUpdateTrip = (trip) => {
+        setTripToUpdate(trip);
+        console.log(trip);
+      };
+    
+      const updateOn = () => {
+        setUpdateActive(true);
+      };
+    
+      const updateOff = () => {
+        setUpdateActive(false);
+      };
+    
+    const fetchTrips = () => {
+        fetch("http://localhost:3000/trip", {
+          method: "GET",
+          headers: new Headers({
+            "Content-Type": "application/json",
+            Authorization: props.token,
+          }),
+        })
+          .then((res) => res.json())
+          .then((tripData) => setTrips(tripData));
+      };
+
+  useEffect(() => {
+    fetchTrips();
+  }, []);
+
+
+  return (
+    <Row>
+      <Col md="3">
+        <TripCreate
+          token={props.token}
+          fetchTrips={fetchTrips}
+          api_key={props.api_key}
+        />
+      </Col>
+      <Col md="9">
+        <TripTable
+          trips={trips}
+          setTripForDestinations={props.setTripForDestinations}
+          tripForDestinations={props.tripForDestinations}
+          editUpdateTrip={editUpdateTrip}
+          updateOn={updateOn}
+          token={props.token}
+          fetchTrips={fetchTrips}
+          api_key={props.api_key}
+        />
+      </Col>
+      {updateActive ? (
+        <TripEdit
+          tripToUpdate={tripToUpdate}
+          updateOff={updateOff}
+          token={props.token}
+          fetchTrips={fetchTrips}
+        />
+      ) : (
+        <></>
+      )}
+    </Row>
+  );
+};
+export default TripIndex;
