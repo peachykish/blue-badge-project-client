06e9e158a08b0030bb130814c3cc517c8dbc9f9d
Harry Dearing <harrysdearing@gmail.com>
2021-02-21 14:26:15 -0500

    Completed Update Fix to store new latitude and longitude when changed and store properly

--- src/trips/TripEdit.js
+++ src/trips/TripEdit.js
@@ -2,38 +2,56 @@ import React,{useState} from 'react';
 import {Button,Label,Form,FormGroup, Input, Modal, ModalBody, ModalHeader} from 'reactstrap'
 
 const TripEdit=(props)=>{
-    console.log('Edit Props', props);
     const [editDesc,setEditDesc]=useState(props.tripToUpdate.description)
     const [editPlace,setEditPlace]=useState(props.tripToUpdate.place)
+    const [lat,setLat]=useState(0.00000);
+    const [lon,setLon]=useState(0.00000);
 
+    const fetchEditTrips = (e) => {
+        e.preventDefault();
+        const api_key='5ae2e3f221c38a28845f05b60282c4994410542a110ccc5c7ff90aa1';
 
-    const tripUpdate=(event,trip)=>{
-        event.preventDefault();
+        fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=${editPlace}&apikey=${api_key}`)
+        .then(response=>response.json())
+        .then(data=>{
+            console.log('Is this real data', data);
+            setLat(data.lat);
+            setLon(data.lon);
+            return [data.lat,data.lon]
+        })
+        .then((data)=>{
+            tripUpdate(data[0], data[1])
+        });
+       };
+
+    const tripUpdate=(latitude, longitude)=>{
         fetch(`http://localhost:3000/trip/${props.tripToUpdate.id}`,{
             method:"PUT",
-            body:JSON.stringify({trip:{description : editDesc, place: editPlace}}),
+            body:JSON.stringify({trip:{description : editDesc, place: editPlace, lat: latitude, lon: longitude}}),
             headers: new Headers ({
                 'Content-Type':'application/json',
                 'Authorization':props.token
             })
         })
-        .then(res=>{
+        .then(res=> res.json())
+        .then((editTripData) => {
             props.fetchTrips();
             props.updateOff();
-        })
+            console.log(editTripData);
+        });
     }
 
     return(
         <Modal isOpen={true}>
             <ModalHeader>Edit a trip</ModalHeader>
             <ModalBody>
-                <Form onSubmit={tripUpdate}>
+                <Form type="submit" onSubmit={fetchEditTrips}>
                     <FormGroup>
-                        <Label htmlFor="description">Edit Description:</Label>
+                        <Label htmlFor="description">Edit the Reason:</Label>
                         <Input name="description" value={editDesc} onChange={(e)=>setEditDesc(e.target.value)}/>
                     </FormGroup>
                     <FormGroup>
-                        <Label htmlFor="place">Edit Definition:</Label>
+                        <Label htmlFor="place">Edit the Place:</Label>
                         <Input name="place" value={editPlace} onChange={(e)=>setEditPlace(e.target.value)}/>
                     </FormGroup>
                     <Button type="submit">Update</Button>
