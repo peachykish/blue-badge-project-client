d6285f9a8211b6956e894b489f601e20a35a1c57
Harry Dearing <harrysdearing@gmail.com>
2021-03-06 13:08:05 -0500

    Fix .env file in client

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -3,7 +3,7 @@ let APIURL = ''
 switch (window.location.hostname) {
     case 'localhost':
     case '127.0.0.1':
-        APIURL = 'http://localhost:5432'
+        APIURL = 'http://localhost:3000'
         break
     case 'jhkn-my-travelapp-client':
         APIURL = 'https://jhkn-my-travelapp.herokuapp.com'
